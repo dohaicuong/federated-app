a46b7ad0a608325f5dc165e9f0991c9871e2d37e
dohaicuong <beatyshot@gmail.com>
2020-01-22 12:59:41 +1000

    make gateway to wait for others service before start up

--- gateway/entrypoint.sh
+++ gateway/entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 if [[ $? == 0 ]]; then
-  /wait-for-it.sh chat-service:4001 -- echo "chat service is up"
   cd /etc/app
-  yarn dev
+  /wait-for-it.sh chat-service:4001 --timeout=5 --strict -- yarn dev
 fi
