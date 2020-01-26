6060d4d02263f420e10954b9aff1e2b17839f8b0
dohaicuong <beatyshot@gmail.com>
2020-01-26 20:21:38 +1000

    add add new service to gateway

--- gateway/src/serviceList.ts
+++ gateway/src/serviceList.ts
@@ -3,4 +3,5 @@ export default [
   // { name: 'chat', endpoint: 'http://localhost:4001/' },
   { name: 'user', endpoint: 'http://user-service:4000/' },
   { name: 'chat', endpoint: 'http://chat-service:4001/', subscription: 'ws://chat-service:4001/graphql' },
+  { name: 'visualize', endpoint: 'http://visualize-service:4002', subscription: 'ws://visualize-service:4002/graphql' }
 ]
